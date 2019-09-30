cd0de1515c5f5b6c6388d92c959f00a8f2e1d779
2B Modular Info <gab.apolinario@hotmail.com>
2019-09-30 19:07:35 -0300

    Upando projeto

---
+++ projetoModelo_A/app/src/main/java/br/com/CalebBiel/projetoModelo_A/Login1Activity.java
@@ -0,0 +1,14 @@
+package br.com.CalebBiel.projetoModelo_A;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login1);
+    }
+}
